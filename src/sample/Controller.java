31864958e7ab0dd8bf4829cbb20843bc6e824af9
johan <johan.windahl@gmail.com>
2018-01-24 10:15:08 +0100

    Remodelled skeleton GUI code from scratch

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,11 +1,5 @@
 package sample;
-import java.util.List;
-import java.util.ArrayList;
 
 
 public class Controller {
-
-    List<Product> listOfProducts = new ArrayList<Product>();
-
-
 }
--- src/sample/Main.java
+++ src/sample/Main.java
@@ -1,18 +1,49 @@
 package sample;
 
 import javafx.application.Application;
+import javafx.event.EventHandler;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
+import java.awt.event.ActionEvent;
+
 public class Main extends Application {
+    Button getButton;
+    Button setButton;
+    Button exitButton;
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 600, 600));
+        //Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+        primaryStage.setTitle("scrubIT");
+
+        getButton = new Button();
+        getButton.setText("Get");
+        getButton.setOnAction(e -> System.out.println("get"));
+
+        setButton = new Button();
+        setButton.setText("Set");
+        setButton.setOnAction(e -> System.out.println("set"));
+
+        exitButton = new Button();
+        exitButton.setText("Exit");
+        exitButton.setOnAction(e -> System.exit(1));
+
+
+
+
+        StackPane layout = new StackPane();
+        layout.getChildren().add(getButton);
+        layout.getChildren().add(setButton);
+        layout.getChildren().add(exitButton);
+
+        Scene root = new Scene(layout, 600, 600);
+
+        primaryStage.setScene(root);
         primaryStage.show();
     }
 
@@ -20,4 +51,8 @@ public class Main extends Application {
     public static void main(String[] args) {
         launch(args);
     }
+
+
+
+
 }
