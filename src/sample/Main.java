6543b9efd820f09bd48a9452866f23ae3eb5c247
johan <johan.windahl@gmail.com>
2018-01-24 10:55:31 +0100

    Added all buttons and a label, with proper placement and action handlers with lamda functions.

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -3,9 +3,12 @@ package sample;
 import javafx.application.Application;
 import javafx.event.EventHandler;
 import javafx.fxml.FXMLLoader;
+import javafx.geometry.Pos;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.layout.Pane;
 import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
@@ -15,31 +18,56 @@ public class Main extends Application {
     Button getButton;
     Button setButton;
     Button exitButton;
+    Label label;
 
     @Override
     public void start(Stage primaryStage) throws Exception{
         //Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
         primaryStage.setTitle("scrubIT");
 
-        getButton = new Button();
-        getButton.setText("Get");
-        getButton.setOnAction(e -> System.out.println("get"));
 
-        setButton = new Button();
-        setButton.setText("Set");
-        setButton.setOnAction(e -> System.out.println("set"));
+
 
         exitButton = new Button();
         exitButton.setText("Exit");
-        exitButton.setOnAction(e -> System.exit(1));
+        exitButton.setOnAction(e -> {
+            System.out.println("Closing");
+            label.setText("Closing");
+            System.exit(1);
+        });
 
+        exitButton.setTranslateX(275);
+        exitButton.setTranslateY(280);
+
+        setButton = new Button();
+        setButton.setText("Set");
+        setButton.setAlignment(Pos.BASELINE_LEFT);
+        setButton.setOnAction(e -> {
+            System.out.println("set");
+            label.setText("set");
+        });
+        setButton.setTranslateX(exitButton.getTranslateX()-40);
+        setButton.setTranslateY(exitButton.getTranslateY());
+
+        getButton = new Button();
+        getButton.setText("Get");
+        getButton.setOnAction(e -> {
+            System.out.println("get");
+            label.setText("get");
+        });
+
+        getButton.setTranslateX(setButton.getTranslateX()-40);
+        getButton.setTranslateY(setButton.getTranslateY());
+
+        label = new Label();
+        label.setText("label text");
+        label.setTranslateY(getButton.getTranslateY());
 
 
 
         StackPane layout = new StackPane();
-        layout.getChildren().add(getButton);
-        layout.getChildren().add(setButton);
-        layout.getChildren().add(exitButton);
+        layout.getChildren().addAll(getButton, setButton,exitButton,label);
+        layout.setAlignment(Pos.CENTER);
 
         Scene root = new Scene(layout, 600, 600);
 
