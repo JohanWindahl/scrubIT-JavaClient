90015bf38b8353423a5c32c6d0f158ee5f7618ed
JohanWindahl <windahl.windahl_lindqvist.7379@student.uu.se>
2018-01-26 10:46:34 +0100

    Added simple http request function to doGetData

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -11,6 +11,9 @@ import javafx.scene.control.Label;
 import javafx.scene.layout.Pane;
 import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
+import java.net.*;
+import java.io.*;
+
 
 import java.awt.event.ActionEvent;
 
@@ -23,47 +26,45 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception{
         //Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("scrubIT");
-
-
-
+        primaryStage.setTitle("scrub IT");
 
         exitButton = new Button();
         exitButton.setText("Exit");
+        exitButton.setTranslateX(275);
+        exitButton.setTranslateY(280);
         exitButton.setOnAction(e -> {
             System.out.println("Closing");
             label.setText("Closing");
             System.exit(1);
         });
 
-        exitButton.setTranslateX(275);
-        exitButton.setTranslateY(280);
-
         setButton = new Button();
         setButton.setText("Set");
-        setButton.setAlignment(Pos.BASELINE_LEFT);
-        setButton.setOnAction(e -> {
-            System.out.println("set");
-            label.setText("set");
-        });
         setButton.setTranslateX(exitButton.getTranslateX()-40);
         setButton.setTranslateY(exitButton.getTranslateY());
+        setButton.setOnAction(e -> {
+            System.out.println("Setting data...");
+            label.setText("Data set.");
+            doSetData();
+        });
 
         getButton = new Button();
         getButton.setText("Get");
-        getButton.setOnAction(e -> {
-            System.out.println("get");
-            label.setText("get");
-        });
-
         getButton.setTranslateX(setButton.getTranslateX()-40);
         getButton.setTranslateY(setButton.getTranslateY());
+        getButton.setOnAction(e -> {
+            System.out.println("Getting data...");
+            label.setText("Getting data...");
+            try {
+                doGetData();
+            } catch (IOException e1) {
+                e1.printStackTrace();
+            }
+        });
 
         label = new Label();
         label.setText("label text");
         label.setTranslateY(getButton.getTranslateY());
-
-
 
         StackPane layout = new StackPane();
         layout.getChildren().addAll(getButton, setButton,exitButton,label);
@@ -75,12 +76,43 @@ public class Main extends Application {
         primaryStage.show();
     }
 
+    private void doSetData() {
+
+    }
+
+    private void doGetData() throws IOException {
+        URL url = null;
+
+        try {
+            url = new URL("https://scrubit.herokuapp.com/joe");
+
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        }
+        BufferedReader in = null;
+        try {
+            in = new BufferedReader(
+                    new InputStreamReader(url.openStream()));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        String inputLine;
+        if (in != null) {
+            while ((inputLine = in.readLine()) != null) {
+                System.out.println(inputLine);
+            }
+        }
+        try {
+            in.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
 
     public static void main(String[] args) {
         launch(args);
     }
-
-
-
 
 }
